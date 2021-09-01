42cec79b0119a6f4daa5198f96b7b3f45f787037
multicypher <multicypher@gmail.com>
2021-09-01 20:26:30 +0530

    added inner page and typography

---
+++ innerpage.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>DigiExpert : Portfolio Template</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top menu shadow">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/images/logo.png" class="img-fluid" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+
+                    <li class="nav-item d-lg-none d-sm-none d-md-none d-xl-none">
+                        <a class="nav-link" href="#">contact</a>
+                    </li>
+                </ul>
+
+                <button type="button" class="d-none d-sm-block d-md-block d-lg-block d-xl-block rounded-pill btn-rounded">
+                    <span>Contact</span>
+                </button>
+            </div>
+        </div>
+    </nav>
+    <!-- main content starts here -->
+    <div id="inner-header" class="inner-header">
+        <div class="container">
+            <div class="row">
+                <h1 class="display-3">About Page</h1>
+            </div>
+        </div>
+    </div>
+    <div id="inner-content" class="inner-content">
+        <div class="container">
+            <div class="row">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quae ratione asperiores, aut beatae similique maxime minus dolores nisi tempora dolor obcaecati rem magnam vel consequatur deserunt nemo voluptates autem?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quae ratione asperiores, aut beatae similique maxime minus dolores nisi tempora dolor obcaecati rem magnam vel consequatur deserunt nemo voluptates autem?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quae ratione asperiores, aut beatae similique maxime minus dolores nisi tempora dolor obcaecati rem magnam vel consequatur deserunt nemo voluptates autem?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quae ratione asperiores, aut beatae similique maxime minus dolores nisi tempora dolor obcaecati rem magnam vel consequatur deserunt nemo voluptates autem?</p>
+                <h2>This is subheading</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum repudiandae error explicabo voluptate! Tenetur ducimus necessitatibus corporis quaerat quisquam voluptas, tempora magni mollitia, in, voluptatum eaque eveniet officia ratione blanditiis!</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+
+    <!-- Footer section -->
+    <div id="footer" class="footer">
+        <div class="container">
+            <div class="row pt-5 pb-5">
+                <div class="col-md-6">
+                    <h2 class="display-2__title fw-bold lh-md">Talk About your project</h2>
+
+                    <ul class="contact-list">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone-calling" width="36" height="36" viewBox="0 0 24 24" stroke-width="1" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                                <line x1="15" y1="7" x2="15" y2="7.01" />
+                                <line x1="18" y1="7" x2="18" y2="7.01" />
+                                <line x1="21" y1="7" x2="21" y2="7.01" />
+                            </svg>
+                            +1 258 965
+                        </li>
+
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="36" height="36" viewBox="0 0 24 24" stroke-width="1" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <rect x="3" y="5" width="18" height="14" rx="2" />
+                                <polyline points="3 7 12 13 21 7" />
+                            </svg>
+                            info@gmail.com
+                        </li>
+
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="36" height="36" viewBox="0 0 24 24" stroke-width="1" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <circle cx="12" cy="11" r="3" />
+                                <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                            </svg>
+                            125,abc street,India
+                        </li>
+                    </ul>
+
+
+                </div>
+                <div class="col-12 col-lg-6">
+
+                    <h1 class="display-2__subtitle lh-lg">We are ready to take new assignment..</h1>
+
+                    <div class="form w-100 pb-2">
+                        <form class="mt-5 mb-5">
+                            <div class="col-lg-12 mb-3">
+
+                                <input type="text" class="form-control transparent-input form-control-lg" id="name" placeholder="Name" aria-describedby="nameHelp">
+
+                            </div>
+                            <div class="input-group col-lg-12 mb-3">
+                                <div class="col-lg-6 mb-3">
+                                    <input type="email" class="form-control transparent-input form-control-lg" id="email" placeholder="Email" aria-describedby="subjectHelp">
+                                </div>
+                                <span> </span>
+                                <div class="col-lg-6 mb-3">
+                                    <input type="subject" class="form-control transparent-input form-control-lg" placeholder="Subject" id="subject" aria-describedby="subjectHelp">
+                                </div>
+
+                            </div>
+                            <div class="col-lg-12 mb-3">
+                                <input type="text" class="form-control transparent-input form-control-lg" placeholder="Message" id="message" aria-describedby="messageHelp">
+                            </div>
+
+                            <button type="submit" class="btn btn-rounded">Submit</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row text-center text-white pt-5 pb-5">
+                <span>&COPY; 2021 All Right Reserved <a href="">DigiExpert</a></span>
+
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
